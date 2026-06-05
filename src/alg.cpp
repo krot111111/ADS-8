6b31832ed2a289a765b8c7422d1597a9e970804f
krotolik2020-ctrl <Kro.tolik2020@gmail.com>
2026-06-05 20:47:37 +0300

    krotov ads8

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
-#include <algorithm>
-#include <cctype>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <cctype>
 #include <string>
-#include <utility>
 #include <vector>
+#include <algorithm>
+#include <utility>
+#include "bst.h"
 
 void makeTree(BST<std::string> &tree, const char *filename) {
   std::ifstream inputFile(filename);
